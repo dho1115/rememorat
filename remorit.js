f4369b1f0f4a4f5e1e87dedb26997206b7489904
David H <davidhorph@hotmail.com>
2018-07-07 13:00:41 -0500

    documentation and centering Input.

--- remorit.js
+++ remorit.js
@@ -1,38 +1,43 @@
 
 
+//Time remaining.
 let span = document.querySelector("span");
+//The div box surrounding the image.
 let questionairre = document.querySelector(".questionairre");
+//The question for the image shown.
 let thequestion = document.querySelector("#question");
+//The "I"mage that is inside the ".questionairre" div.
 let I = document.querySelector("#I");
+//The input box where the user inputs the answer.
+let input = document.querySelector(".input");
+//The counter for the timer.
 let count = 0;
 let randNum = Math.floor(Math.random() * 35791457915);
+
+questionairre.classList.add("TimeIsUp");
 
 let timer = setInterval(function() {       
     if(count == 8) {        
         clearInterval(1);
         span.textContent = "TIME IS UP!!!";
-        thequestion.textContent = NumberMemory("Q");
+        thequestion.textContent = numberQuestion.Question;
         questionairre.classList.add("TimeIsUp");
     } else {
+        questionairre.classList.remove("TimeIsUp");
         count +=1;
-        I.textContent = NumberMemory("I");
+        I.textContent = numberQuestion.Image;
         span.textContent = 8-count;
     }
 },1000)
 
 //Have an object. set key value pairs with one key/value being the picture on the screen and the second one being the question AFTER the picture has left. 
 
-function NumberMemory(IorQ) {
-    let obj = {};
-    obj.img = randNum;
-    obj.question = "What was the number on the screen?"
+let numberQuestion = {
+    Image: randNum,
+    Question: "What was the number on the screen?"
+}
 
-    if(IorQ == "I") {
-        return obj.img;
-    } else {
-        return obj.question;
-    }
-} 
+
 
 
 
