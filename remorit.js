5edca48ff0256646e9128c803d07c6e442acf66e
David H <davidhorph@hotmail.com>
2018-07-07 09:27:33 -0500

    Changes...
    (1) Added First Img..
    (2) Added Question to go along with that image.
    
    Haven't added question box (at least not yet).

--- remorit.js
+++ remorit.js
@@ -2,16 +2,37 @@
 
 let span = document.querySelector("span");
 let questionairre = document.querySelector(".questionairre");
+let thequestion = document.querySelector("#question");
+let I = document.querySelector("#I");
 let count = 0;
+let randNum = Math.floor(Math.random() * 35791457915);
 
-let timer = setInterval(function() {    
-    if(count == 16) {
+let timer = setInterval(function() {       
+    if(count == 8) {        
         clearInterval(1);
+        span.textContent = "TIME IS UP!!!";
+        thequestion.textContent = NumberMemory("Q");
         questionairre.classList.add("TimeIsUp");
     } else {
         count +=1;
+        I.textContent = NumberMemory("I");
+        span.textContent = 8-count;
     }
-    span.textContent = 16-count;
 },1000)
 
-//Have an object. set key value pairs with one key/value being the picture on the screen and the second one being the question AFTER the picture has left.
+//Have an object. set key value pairs with one key/value being the picture on the screen and the second one being the question AFTER the picture has left. 
+
+function NumberMemory(IorQ) {
+    let obj = {};
+    obj.img = randNum;
+    obj.question = "What was the number on the screen?"
+
+    if(IorQ == "I") {
+        return obj.img;
+    } else {
+        return obj.question;
+    }
+} 
+
+
+
