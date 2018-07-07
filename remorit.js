2b20724a5319d4075453a907fffc23dbd4af807b
David H <davidhorph@hotmail.com>
2018-07-07 01:29:44 -0500

    ...

--- remorit.js
+++ remorit.js
@@ -1,10 +1,17 @@
 
 
 let span = document.querySelector("span");
+let questionairre = document.querySelector(".questionairre");
 let count = 0;
 
-let timer = setInterval(function() {
-    count +=1;
-    span.textContent = count;
-},5000)
+let timer = setInterval(function() {    
+    if(count == 16) {
+        clearInterval(1);
+        questionairre.classList.add("TimeIsUp");
+    } else {
+        count +=1;
+    }
+    span.textContent = 16-count;
+},1000)
 
+//Have an object. set key value pairs with one key being the 
