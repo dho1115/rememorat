c7c29af4f5893b3ffcf0b7b441ac3cbfda5ff091
David H <davidhorph@hotmail.com>
2018-07-07 01:35:22 -0500

    ...

--- remorit.js
+++ remorit.js
@@ -14,4 +14,4 @@ let timer = setInterval(function() {
     span.textContent = 16-count;
 },1000)
 
-//Have an object. set key value pairs with one key being the 
+//Have an object. set key value pairs with one key/value being the picture on the screen and the second one being the question AFTER the picture has left.
